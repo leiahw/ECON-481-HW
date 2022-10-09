8ce8c235c2ab98bd1e3fcccc883da02f84de0f8d
leiahw <leia.hwangbo@gmail.com>
2022-10-09 00:40:49 -0700

    set up inputs

--- Problem Set 1/basicop.py
+++ Problem Set 1/basicop.py
@@ -1,3 +1,8 @@
-bill_acct = 1000
-ir = 0.05
-years = 10
+bill_acct = int(input('Bills starting account value: '))
+ir = float(input('interest rate: '))
+
+print(bill_acct)
+print(ir)
+# years = 10
+# tot_wealth = bill_acct + (ir * 10)
+# print("Bill's total wealth is $", tot_wealth)
